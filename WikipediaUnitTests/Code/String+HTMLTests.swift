6c28dfdf54d03fd9f8c4035083ed167c0c96af59
Joe Walsh <joewalsh@users.noreply.github.com>
2024-05-12 07:58:55 -0400

    add tests for more languages

--- WikipediaUnitTests/Code/String+HTMLTests.swift
+++ WikipediaUnitTests/Code/String+HTMLTests.swift
@@ -21,9 +21,30 @@ class StringHTMLTests: XCTestCase {
         XCTAssertEqual(result, "<b>start</b> <b>12345</b>")
     }
     
-    func testRTL() {
+    func testAR() {
+        let result = "توم هانكس".applyingBoldTag(to: "هانكس")
+        XCTAssertEqual(result, "توم <b>هانكس</b>")
+        let result2 = "توم هانكس".applyingBoldTag(to: "ت")
+        XCTAssertEqual(result2, "<b>ت</b>وم هانكس")
+    }
+    
+    func testHE() {
         let result = "& ג'ולייט".applyingBoldTag(to: "ג'ולייט")
         XCTAssertEqual(result, "& <b>ג'ולייט</b>")
+        let result2 = "& ג'ולייט".applyingBoldTag(to: "&")
+        XCTAssertEqual(result2, "<b>&</b> ג'ולייט")
+    }
+    
+    func testJA() {
+        let result = "トム・ハンクス".applyingBoldTag(to: "ハンクス")
+        XCTAssertEqual(result, "トム・<b>ハンクス</b>")
+    }
+    
+    func testSR() {
+        let result = "Србија".applyingBoldTag(to: "Србија")
+        XCTAssertEqual(result, "<b>Србија</b>")
+        let result2 = "Србија".applyingBoldTag(to: "ија")
+        XCTAssertEqual(result2, "Срб<b>ија</b>")
     }
     
     func testZH() {
